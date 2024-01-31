3070b5d246c1374d4230181815b431b4e5f42ed7
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-01-31 23:00:08 +0000

    Initial commit

---
+++ wintree/wintree/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace wintree
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public XmlDataProvider DataProvider { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataProvider = new XmlDataProvider();
+
+
+            LoadTree();
+
+            this.Left = SystemParameters.PrimaryScreenWidth - this.Width;
+            this.Top = SystemParameters.PrimaryScreenHeight - this.Height;
+        }
+
+
+        private void LoadTree() //Load the XML Tree File
+        {
+
+            try
+            {
+                //Get XML file
+                string file = System.IO.Path.GetFileName(Settings.Default.TreeFile);
+                string path = System.IO.Path.GetDirectoryName(Settings.Default.TreeFile);
+
+                if (path == "")
+                    path = Directory.GetCurrentDirectory();
+
+                //Initiliaze XmlDataProvider 
+                DataProvider.Source = new Uri(path + @"\" + file);
+                DataProvider.XPath = "/Tree";
+                this.treeview.DataContext = DataProvider;
+
+                //Add FileSystemWatcher to update treeview on change
+                FileSystemWatcher fsWatcher = new FileSystemWatcher(path, file); //path, filter
+                fsWatcher.NotifyFilter = NotifyFilters.LastWrite;
+                fsWatcher.Changed += new FileSystemEventHandler(fsWatcher_Changed);
+                fsWatcher.EnableRaisingEvents = true;
+
+            }
+
+
+            catch (Exception e)
+            {
+                MessageBox.Show("Problem loading XML Tree File: \n\n" + e.ToString());
+            }
+
+        }
+
+
+        private (string, string, string) GetSelectedItem()
+        {
+            try
+            {
+                XmlElement xmlElement = (XmlElement)treeview.SelectedItem;
+
+                string name = (xmlElement.GetAttribute("Name").ToString());
+                string command = (xmlElement.GetAttribute("Command").ToString());
+                string parameters = (xmlElement.GetAttribute("Parameters").ToString());
+
+
+                return (name, command, parameters);
+
+            }
+
+
+            catch (Exception e)
+            {
+                MessageBox.Show("Something went wrong:\n" + e.Message);
+                throw new Exception("Couldn't get item");
+
+
+            }
+
+        }
+
+
+
+
+        void fsWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            DataProvider.Refresh();
+        }
+
+
+        private void Launch_Click(object sender, RoutedEventArgs e)
+        {
+
+            var (name, command, parameters) = GetSelectedItem();
+            var session = new Session(name, command, parameters);
+
+        }
+
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".xml";
+            dlg.Filter = "XML Files (*.xml)|*.xml";
+            bool? result = dlg.ShowDialog();
+
+
+            if ((bool)result)
+            {
+                Settings.Default.TreeFile = dlg.FileName;
+                Settings.Default.Save();
+                LoadTree();
+            }
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(Settings.Default.Editor, Settings.Default.TreeFile);
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            DataProvider.Refresh();
+        }
+
+        private void Log_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(Settings.Default.Editor, Settings.Default.LogFile);
+        }
+
+
+        private void OnTop_Click(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = !(this.Topmost);
+
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("wintree v1.0 \n https://github.com/dmaccormac");
+        }
+
+
+        private void Item_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var (name, command, parameters) = GetSelectedItem();
+            var session = new Session(name, command, parameters);
+        }
+
+
+    }
+}
---
+++ wintree/wintree/Session.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wintree
+{
+    class Session
+    {
+        private string name;
+        private string command;
+        private string parameters;
+
+
+        public Session(string name, string command, string parameters)
+        {
+            this.name = name;
+            this.command = command;
+            this.parameters = parameters;
+
+            this.Start();
+
+        }
+
+
+        public void Start()
+        {
+
+            try
+            {
+
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo(command, parameters) { UseShellExecute = false, RedirectStandardOutput = true }
+                };
+
+                process.Start();
+                WriteLog("[" + DateTime.Now + "] Start " + name + " (" + command + ")");
+
+                var outputResultPromise = process.StandardOutput.ReadToEndAsync();
+                outputResultPromise.ContinueWith(o => WriteLog("[" + DateTime.Now + "] End " + name + " (" + command + ")"));
+
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show("Something went wrong: \n" + e.Message);
+
+
+            }
+
+
+        }
+
+
+        private int WriteLog(string s)
+        {
+
+            try
+            {
+                string filename = Settings.Default.LogFile;
+                File.AppendAllText(filename, s + Environment.NewLine);
+
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show("Something went wrong: \n" + e.Message);
+                return -1;
+
+            }
+
+            return 0;
+
+
+
+        }
+
+    }
+}
